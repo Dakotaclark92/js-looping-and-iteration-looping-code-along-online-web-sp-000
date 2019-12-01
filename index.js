eb5bc637ef08dac2295d6af2423a7532ce837656
Dakota Clark <dakotaclark92@yahoo.com>
2019-12-01 02:57:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-const names = ['Lisa', 'Kaitlin', 'Jan'];
-
-function writeNames (names) { for (let i = 0; i < names.length; i++)
-{console.log(`Thank you, ${names[i]} for the wonderful surprise gift!`);
+function writeCards(names, event) {
+  let messages = [];
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return messages;
 }
 
-return names;
+function countDown(number) {
+  while(number >= 0) {
+    console.log(number);
+    number--;
+  }
 }
-
-writeNames(names);
